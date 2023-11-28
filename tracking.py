80abcc9a34c2e4fe5b6c7585080a4327ce31ec29
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-28 17:23:06 +0300

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -8,20 +8,7 @@ from PIL import Image
 from ultralytics import YOLO
 import cv2
 
-# from roboflow import Roboflow
 
-# rf = Roboflow(api_key="YKinp3U6gaRntggDUVVq")
-# project = rf.workspace().project("tank-detect")
-# tank_model = project.version(3).model
-
-# # # infer on a local image
-# # print(model.predict("your_image.jpg", confidence=40, overlap=30).json())
-
-# # # visualize your prediction
-# # model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
-
-# # # infer on an image hosted elsewhere
-# # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
 
 def load_config_data(data_path: str) -> dict: 
     """Функция для загрузки всех данных из конфигурационного файла yaml"""
