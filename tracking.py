30380f83d13fbb6efafcb46d0104433b2501c30c
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-29 16:23:48 +0300

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -10,39 +10,6 @@ import cv2
 
 
 
-def load_config_data(data_path: str) -> dict: 
-    """Функция для загрузки всех данных из конфигурационного файла yaml"""
-    try:
-        with open(data_path, 'r', encoding='utf8') as file:
-            config_data = yaml.safe_load(file)
-            return config_data
-        
-    except FileNotFoundError:
-        print('Ошибка при загрузке конфигурационного файла')
-
-
-# Загрузка всех переменных из конфигурационного файла
-CONFIG_DATA = load_config_data('config.yaml')
-print(CONFIG_DATA)
-
-results = model.train(
-   data=CONFIG_DATA['data'],  # Path to the YAML file
-   imgsz=CONFIG_DATA['imgsz'],  # Image resize
-   epochs=CONFIG_DATA['epochs'],  # The number of epochs
-   batch=CONFIG_DATA['batch'],  # The number of batch during train
-   name=CONFIG_DATA['name']  # The name of trained model
-   )
-
-try:
-    model = YOLO(CONFIG_DATA['yolov8_model'])
-    model.to(device=torch.device("cuda"))
-    print('Модель YOLO успешно загружена')
-    print(f'Девайс для YOLO: {model.device}')
-    
-except Exception:
-    print('Возникла ошибка при загрузке модели')
-
-
 
 
 # Open the video file
