10fc08a8c1f14664ad4eb35afd315edb7571f489
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-11-29 16:22:59 +0300

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -97,3 +97,34 @@ while cap.isOpened():
 # Release the video capture object and close the display window
 cap.release()
 cv2.destroyAllWindows()
+
+
+# In this case we can use Pipeline (inner and outer), ColumnTransformer, custom classes, custom def function in Pipeline, bu we selected simple example to show OptunaSearchCV
+
+# KFold strategy
+kf = KFold(n_splits=5, shuffle=True, random_state=50)
+
+
+# Define the model
+clf = CatBoostClassifier(verbose=False)
+
+
+# Define param distribution
+# IMPORTANT: IN param_distrs we can use only optuna.distribution! For instance, we can't use list, np.array and e.t.c.
+param_distrs = {
+                'min_data_in_leaf': optuna.distributions.IntDistribution(1, 10),
+                'iterations': optuna.distributions.IntDistribution(800, 1200, 100),
+                }
+
+
+# OptunaSearchCV 
+opt_search = optuna.integration.OptunaSearchCV(clf,
+                                               param_distrs,
+                                               cv=kf,
+                                               scoring='f1',
+                                               n_trials=10,  # Important parameters! In total we have 10 combination of hyperparameters and it's all
+                                               timeout=100)  # Important parameters! In total trial time = 100 second
+
+
+# Let's get started 
+opt_search.fit(X_train, y_train)
