8efb105f33f60e8f53eca46847cc42f8741a8fb0
Ali Sultanov <ali.sultanov.2024@mail.ru>
2023-12-06 17:24:27 +0300

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -2,10 +2,16 @@ import yaml
 import torch
 from time import time   
 import numpy as np
+
+from military_drone_display import Display
 from PIL import Image
 from ultralytics.utils.plotting import Annotator, colors
 from ultralytics import YOLO
 import cv2
+
+
+# Важная ссылка
+#  YOUTUBE: Live tracking on real FPV drone video | Autonomous Drone Object Tracking OpenCV Python   - отображение указателя 
 
 
 class YoloModel:
@@ -16,7 +22,7 @@ class YoloModel:
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model_path = model_path
         self.model = self.load_model()
-        self.CLASS_NAMES_DICT = self.model.model.names
+        self.CLASS_NAMES_DICT = self.model.names
         self.start_time = 0
         self.end_time = 0
 
@@ -28,6 +34,23 @@ class YoloModel:
         model = model.to(device='cpu')
         model.fuse()
         return model
+    
+
+    def box_plotting(self, results):
+        """Отображение на экране box-а c распознанными объектами"""
+
+        # Получение всех боксов
+        boxes = results[0].boxes.xywh.cpu()
+        # Визуализация результата на фрейме
+        annotated_frame = results[0].plot()
+        # Отрисовка бокса с указанием id трека 
+        for box in boxes:
+            x, y, w, h = box
+            x, y, w, h = int(x), int(y), int(w), int(h)
+            # Отрисовка центров для Bounding Boxes
+            cv2.drawMarker(annotated_frame, (x, y), (0, 0, 255), cv2.MARKER_CROSS, 15, 2)
+
+        return annotated_frame
 
 
     def save_model(self):
@@ -40,31 +63,24 @@ class YoloModel:
         pass
 
 
-class Display:
-    """Класс для отображения дополнительной информации о работе трекера на экран"""
 
-    def fps_display(self, im0) -> None:
-        """Отображение на экран FPS"""
-        self.end_time = time()
-        fps = 1 / np.round(self.end_time - self.start_time, 2)
-        text = f'FPS: {int(fps)}'
-        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1.0, 2)[0]
-        gap = 10
-
-        cv2.rectangle(im0, (1130 - gap, 50 - text_size[1] - gap), (1130 + text_size[0] + gap, 50 + gap), (56, 84, 41), -1)
-        cv2.putText(im0, text, (1130, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2)
-    
-
-    def starting_time_display(self, im0) -> None: 
-        """Отображение на экран времени работы"""
-        pass
-
-    def logotype_display(self, im0) -> None:
-        """Отображение логотипа"""
-        pass
+# Добавить функционал изменения размера изображения
+# Добавить функционал как из видео на YOUTUBE
+# Перевести в отдельный проект
+# Вынести отображение дисплея в отдельный класс ? + 
+# Создать ipynb для EDA датасета
+# ToDo сделать метод box_plot (как на изображении, так и на видео) +
+# Пофиксить костыль с try-except + 
+# Пофиксить отображение FPS / Чекнуть правильность +
+# Изменить указатель объекта (вместо точки) +
+# Заполнить save_model, test_model
+# Перевести все комментарии на русский язык +
+# Перевести отображение класса на русский язык 
+# Добавить отображение времени работы трекера
+# Разобраться с colors
 
 
-    
+
 class YoloTracker(YoloModel, Display):
     """Класс YOLOv8 для задачи трекинга объектов"""
 
@@ -74,15 +90,16 @@ class YoloTracker(YoloModel, Display):
 
     def image_tracking(self, image_path: str) -> None: 
         """Предсказание модели на изображении"""
-
-        results = self.model.track(image_path, persist=True)  # Cписок с результатами
-        # Отображение результатов
-        for result in results:
-            im_array = result.plot()  # Список BGR в numpy array 
-            im = Image.fromarray(im_array[..., ::-1])  # Переводим в RGB PIL image
-            im.show()  # Отображение изображения
-
-    
+        
+        # Список с результатами
+        results = self.model.track(image_path, persist=True)
+        # Отображение на экран
+        annotated_frame = self.box_plotting(results)
+        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+ 
+        
     def stream_tracking(self, video_path: str) -> None: 
         """Предсказание модели на видеопотоке"""
 
@@ -90,6 +107,7 @@ class YoloTracker(YoloModel, Display):
         cap = cv2.VideoCapture(video_path)
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        
         frame_count = 0
         assert cap.isOpened()
 
@@ -102,24 +120,14 @@ class YoloTracker(YoloModel, Display):
             if success:
                 # Запуск YOLOv8 трекера
                 results = self.model.track(frame, persist=True)
-                # Получение всех боксов
-                boxes = results[0].boxes.xywh.cpu()
-        
-                # Визуализация результата на фрейме
-                annotated_frame = results[0].plot()
-        
-                # Отрисовка бокса с указанием id трека 
-                for box in boxes:
-                    x, y, w, h = box
-                    x, y, w, h = int(x), int(y), int(w), int(h)
-        
-                    # Отрисовка центров для Bounding Boxes
-                    cv2.circle(annotated_frame, (x, y), 5, (0, 0, 255), -1)
-        
+                # Отображение на экране box-ов
+                annotated_frame = self.box_plotting(results) 
+
                 # Вывод fps, времени работы, эмблемы на дисплей
                 self.fps_display(annotated_frame)
                 self.starting_time_display(annotated_frame)
                 self.logotype_display(annotated_frame)
+                self.target_aiming_display(annotated_frame, 640, 360)
 
                 # Вывод кадра на экран
                 cv2.imshow("YOLOv8 Tracking", annotated_frame)
@@ -127,12 +135,12 @@ class YoloTracker(YoloModel, Display):
                 frame_count += 1
                 # Остановка цикла при нажатии "q"
                 if cv2.waitKey(1) & 0xFF == ord("q"):
-                     break
+                    break
             else:
-                # Остановка цикла если видео закончено
+                # Остановка цикла, если видео закончено
                 break
         
-        # Закрытие видео отображения
+        # Закрытие видеопотока 
         cap.release()
         cv2.destroyAllWindows()
 
@@ -145,7 +153,8 @@ if __name__ == '__main__':
     print(f'Загружена модель: {tracker.model_path}')
     print(f'Используемый девайс: {tracker.device}')
 
-    # # Запуск на трекиг видеопотока
+    # Запуск на трекиг видеопотока
     tracker.stream_tracking(r"C:\Users\1NR_Operator_33\Downloads\танк Т90м Прорыв лучший танк в мире в бою.mp4")
+    # tracker.image_tracking(r'D:\tank_detection\valid\images\sideup-30-_jpg.rf.99262d69f67dd081581b23af2d553ca2.jpg')
 
 
