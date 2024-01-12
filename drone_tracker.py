a1eaf0275f614738d2be0b2e72a942068c544bda
Ali Sultanov <ali.sultanov.2024@mail.ru>
2024-01-12 16:15:22 +0300

    Update drone_tracker.py

--- drone_tracker.py
+++ drone_tracker.py
@@ -11,90 +11,57 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
 
-class YoloModel:
-    """Базовый класс для работы с YOLOv8"""
-
-    def __init__(self, model_path):
-
-        self.device = 'cuda' if is_available() else 'cpu'
-        self.model_path = model_path
-        self.model = self.load_model()
-        self.CLASS_NAMES_DICT = self.model.names
-
-        
-    def load_model(self):
-        """Загрузка модели YOLOv8"""
-
-        model = YOLO(self.model_path)
-        model = model.to(device='cpu')
-        model.fuse()
-        return model
 
 
-    def train_model(self, data, imgsz, epochs, batch, name):
-        """Тренировка модели YOLOv8 для задачи трекинга"""
+# Open the video file
+video_path = r"C:\Users\1NR_Operator_33\Downloads\Поток машин на ТТК (Москва), весна-лето 2018.mp4"
+cap = cv2.VideoCapture(video_path)
 
-        self.model.train(
-        data=data,  # Путь до YAML файла
-        name=name,  # Название модели, которая сохранится 
-        imgsz=imgsz,  
-        epochs=epochs,  
-        batch=batch)
-    
+# Store the track history
+track_history = defaultdict(lambda: [])
 
-    def test_model(self):
-        """Запуск на тестирование модели YOLOv8"""
-        # self.model.val()
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
 
-
-    def _box_plotting(self, results):
-        """Отображение на экране box-а c распознанными объектами"""
-    
-        # Получение всех боксов
+    if success:
+        # Run YOLOv8 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+        # Get the boxes and track IDs
         boxes = results[0].boxes.xywh.cpu()
-        # Визуализация результата на фрейме
+        track_ids = results[0].boxes.id.int().cpu().tolist()
+
+
+        # Visualize the results on the frameq
         annotated_frame = results[0].plot()
-        # Отрисовка бокса с указанием id трека 
-        for box in boxes:
+
+        # Plot the tracks
+        for box, track_id in zip(boxes, track_ids):
             x, y, w, h = box
             x, y, w, h = int(x), int(y), int(w), int(h)
-            # Отрисовка центров для Bounding Boxes
-            cv2.drawMarker(annotated_frame, (x, y), (0, 0, 255), cv2.MARKER_CROSS, 15, 2)
+            track = track_history[track_id]
+            track.append((float(x), float(y)))  # x, y center point
+            if len(track) > 30:  # retain 90 tracks for 90 frames
+                track.pop(0)
 
-        return annotated_frame 
-    
+            # Draw the tracking lines
+            points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+            cv2.polylines(annotated_frame, [points], isClosed=False, color=(0, 255, 0), thickness=2)
 
+            # Draw the circle for bounding boxes
+            cv2.circle(annotated_frame, (x, y), 5, (0, 0, 255), -1)
 
-class YoloTracker(YoloModel):
-    """Класс YOLOv8 для задачи трекинга объектов"""
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Tracking", annotated_frame)
 
-    def __init__(self, model_path):
-        super().__init__(model_path)
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
 
-
-    def image_tracking(self, image_path: str) -> None: 
-        """Предсказание модели на изображении"""
-        # Список с результатами
-        results = self.model.track(image_path, persist=True)
-        # Отображение на экран
-        annotated_frame = self._box_plotting(results)
-        cv2.imshow("Sharp eye", annotated_frame)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
- 
-        
-    
-        
-        # Закрытие видеопотока
-        cap.release()
-        cv2.destroyAllWindows()
-  
-
-
-
-
-if __name__ == '__main__':
-    # Cоздание экземпляра класса
-    tracker = YoloTracker(r"C:\Users\1NR_Operator_33\runs\detect\yolov8_tank_detection42\weights\best.pt")
-    logging.info(f'Загружена модель: {tracker.model_path}')
-    logging.info(f'Используемый девайс: {tracker.device}')
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
