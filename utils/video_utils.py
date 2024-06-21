34bda8bcad663c7297db2971944a6d79a3312ddd
Ali Sultanov <ali.sultanov.2024@mail.ru>
2024-06-21 16:10:34 +0300

    Update video_utils.py

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,12 +1,19 @@
 import cv2
 
+
 def get_frame_size(cap):
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) 
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     return frame_width, frame_height
 
-def set_frame_size(cap):
-    pass
+def set_frame_size(cap, frame_width: int, frame_height: int):
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
-def bbox_center(xyxys, idx) -> tuple:
-    return  (float((xyxys[idx][0] + xyxys[idx][2]) // 2), float((xyxys[idx][1]) + xyxys[idx][3]) // 2)
+
+def get_bbox_center(bbox) -> tuple:
+    return (float((bbox[0] + bbox[2]) // 2), float((bbox[1]) + bbox[3]) // 2)
+
+
+def get_bbox_width(bbox):
+    return bbox[2] - bbox[1]
