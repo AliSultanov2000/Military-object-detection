fd72a568480b00b1ff97af856d1cc20085673b63
Ali Sultanov <ali.sultanov.2024@mail.ru>
2024-06-23 18:22:57 +0300

    Update detectron.py

--- model/detectron.py
+++ model/detectron.py
@@ -2,10 +2,10 @@ import torch
 import cv2
 import warnings
 import sys
+import numpy as np
 
 from pathlib import Path
-from dataclasses import dataclass 
-from time import time
+from dataclasses import dataclass, field
 from ultralytics import YOLO, RTDETR
 
 sys.path.append("./")
@@ -23,12 +23,18 @@ class ModelDetectronConfig:
     verbose: bool = True
     run_cuda = True
     device: torch.device = torch.device('cuda' if torch.cuda.is_available() and run_cuda else 'cpu')
+    clsses_weapon: tuple = (0, 2)
+    person_name: str = "человек"
+    armed_person_name: str = "вооруженный человек"
 
-    show_classes: tuple = ()
-
+    colors: dict = field(default_factory=lambda: {
+        ModelDetectronConfig.person_name: (255, 0, 0),
+        ModelDetectronConfig.armed_person_name: (0, 0, 255)
+        })
+    
     save_video: bool = True 
-    saved_video_folder: str = r"output_videos"
-    saved_video_name: str = r"output_video" 
+    saved_video_folder: str = r"videos_output"
+    saved_video_name: str = r"video_output" 
     saved_video_format: str = r".mp4"
     #-----------------------------------------------------------------------------------------
 
@@ -37,8 +43,7 @@ class ModelDetectron(ModelDetectronConfig):
     def __init__(self, model_type: str, model_weights: str):
         self.model = self.load_model(model_type, model_weights)
 
-
-    def load_model(self, model_type: str, model_weights: str) -> RTDETR | YOLO:
+    def load_model(self, model_type: str, model_weights: str) -> YOLO | RTDETR:
         """Возвращает модель: либо YOLO, либо RTDETR"""
 
         model_type = model_type.lower()
@@ -53,8 +58,30 @@ class ModelDetectron(ModelDetectronConfig):
         model = model.to(self.device)
         model.fuse()
         return model
+    
 
+    def check_bboxs_intersection(self, xyxys, cls, person_idx: int) -> bool:
+        """
+        Функция для проверки пересечения bounding boxes
+        человека и оружия с целью выдать метку вооруженный человек 
+        """
+        for i in range(len(xyxys)):
+            if cls[i] in self.clsses_weapon:
+                # Проверяем, пересекаются ли bb person_idx и оружия
+                box1 = xyxys[i]             # bbox оружия
+                box2 = xyxys[person_idx]    # bbox человека
+                b1_x1, b1_y1, b1_x2, b1_y2 = box1
+                b2_x1, b2_y1, b2_x2, b2_y2 = box2
+                # Зона пересечения
+                inter_area = (np.minimum(b1_x2, b2_x2) - np.maximum(b1_x1, b2_x1)).clip(0) * (
+                    np.minimum(b1_y2, b2_y2) - np.maximum(b1_y1, b2_y1)
+                ).clip(0)
 
+                if inter_area != 0:
+                    return True
+        return False 
+ 
+        
     def __call__(self, input_video_path: str):
         cap = cv2.VideoCapture(input_video_path)
 
@@ -65,7 +92,7 @@ class ModelDetectron(ModelDetectronConfig):
         # Сохранение видеопотока
         if self.save_video:
             Path(self.saved_video_folder).mkdir(parents=True, exist_ok=True)
-            
+        
             video_num = latest_video_file_num(ModelDetectronConfig())
             save_path = f'{self.saved_video_folder}/{self.saved_video_name}{video_num}{self.saved_video_format}'
 
@@ -77,39 +104,53 @@ class ModelDetectron(ModelDetectronConfig):
         cls_display = Display(self.frame_width, self.frame_height)
 
         while cap.isOpened():
-            cls_display.start_time = time()
             # Считывание фрейма из видео
             success, frame = cap.read()            
             if not success:
                 break
 
             # Запуск на детекцию
+            #-------------------------------
             results = self.model(frame)
             boxes = results[0].boxes.cpu()
-    
+            #-------------------------------
             if len(boxes) != 0: 
                 # Выход модели для данного frame
-                xyxys = list(boxes.xyxy.numpy().astype(int))
-                cls = list(boxes.cls.numpy().astype(int))
-                conf = list(boxes.conf.numpy().astype(float))
-            
+                #----------------------------------------------
+                xyxys = boxes.xyxy.cpu().numpy().astype(int)
+                cls = boxes.cls.cpu().numpy().astype(int)
+                conf = boxes.conf.cpu().numpy().astype(float)
+                #----------------------------------------------
                 for idx in range(len(xyxys)):
-                    cls_display.draw_boxing(frame, idx, xyxys, cls, f"{self.model.names[cls[idx]]} {conf[idx]:.2f}")
-
+                    if cls[idx] == 1:  # Если это человек
+                        if self.check_bboxs_intersection(xyxys, cls, idx):
+                            #------для доп.визуализации вооруж.человека------
+                            cls_display.draw_ellipse(frame,
+                                                     xyxys[idx])
+                            #------------------------------------------------
+                            person_type = self.armed_person_name
+                        else:  # если нет пересечения
+                            person_type = self.person_name
+                        # Выводим на экран только человека
+                        cls_display.draw_boxing(frame,
+                                                xyxys[idx],
+                                                f'{person_type} {conf[idx]:.2f}',
+                                                ModelDetectronConfig().colors[person_type])
+                        
             if self.save_video:
                 save_out.write(frame)
         
             ret, buffer = cv2.imencode('.jpg', frame)
 
             frame = buffer.tobytes()
-    
+
+            # Передаём результат в сервер
             yield(b'--frame\r\n'
                            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
-        # Сохранение видеопотока в указанную папку
         cap.release()
-
+        
+        # Сохранение видеопотока в указанную папку
         if self.save_video:
             save_out.release()
             logger.info(f"Сохранённый видеопоток: {save_path}")
